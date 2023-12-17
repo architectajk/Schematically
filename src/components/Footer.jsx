5bbd5e58bf660c575ea009f697c797b681264104
architectajk <architect.ajk@gmail.com>
2023-12-17 22:31:34 +0530

    Update NBCPart0.pdf and Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,10 +14,10 @@ export default function Footer() {
           <span className={`mb-3 mb-md-0 text-${mode==='dark'?'light':'dark'}`}>© 2024-2025 Schematically, Inc. All rights reserved.</span>
         </div>
         <div className="container d-flex justify-content-end ">
-        <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
-          <li className="ms-3 "><Link to="https://www.linkedin.com/in/akshay-j-kamath" target="_blank" rel="noopener noreferrer"><FaLinkedin size={25} color="grey"/></Link></li>
-          <li className="ms-3"><Link><FaInstagram size={25} color="grey"/></Link></li>
-        </ul>
+          <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+            <li className="ms-3 "><Link to="https://www.linkedin.com/in/akshay-j-kamath" target="_blank" rel="noopener noreferrer"><FaLinkedin size={25} color="grey"/></Link></li>
+            <li className="ms-3"><Link><FaInstagram size={25} color="grey"/></Link></li>
+          </ul>
         </div>
         </div>
       </footer>
